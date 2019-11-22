17964d7bd1dde2cc95c4791b6c8cdd3f65278d27
spiltmiruku <kitsunedg@gmail.com>
2019-11-21 21:20:47 -0700

    Finished setting up basic files/folders

--- nodbproject/server/index.js
+++ nodbproject/server/index.js
@@ -1,7 +1,13 @@
 const express = require('express'),
     cors = require('cors'),
-    tripsCtrl = require('./controllers/tripsCtrl');
+    tripsCtrl = require('./controllers/tripsCtrl'),
+    gradient = require('gradient-string');
 
     const app = express();
 
-    
+app.use(cors());    
+app.use(express.json());
+
+const PORT = 7777
+
+app.listen(PORT, () => console.log(gradient.rainbow(`Ready for takeoff on runway ${PORT}`)))
--- nodbproject/src/App.js
+++ nodbproject/src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import FlightCard from './Components/FlightCard';
+import Header from './Components/Header';
+import MainPage from './Components/MainPage'
+import UpcomingTrips from './Components/UpcomingTrips';
 
-function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+            onUpcomingTripsPage: false, 
+    };
+  }
+  render() {
+    return <div className="App">
+       <FlightCard />
+       <Header />
+       <MainPage />
+       <UpcomingTrips />
+
+    </div>;
+  }
 }
 
 export default App;
